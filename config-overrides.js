f1831f5ef192bf17e604822852ada09253eafda6
David Anthoff <anthoff@berkeley.edu>
2019-12-05 09:59:54 -0800

    Fully use rewired

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,7 @@ const {
     override,
     setWebpackTarget
   } = require("customize-cra");
-  const path = require("path");
   
   module.exports = override(
-    setWebpackTarget('node')
+    setWebpackTarget('electron-renderer')
   );
